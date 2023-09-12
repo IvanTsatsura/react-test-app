fc3a664b431e101158e3b5d2863e01834a6c3e17
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-09-12 13:48:16 +0200

    Add data and CostItem components to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import CostItem from "./Components/CostItem";
 
 function App() {
+  const costs = [
+    { date: new Date(2022, 12, 9), name: "Fridge", cost: 999 },
+    { date: new Date(2021, 3, 21), name: "IPhone", cost: 1099 },
+    { date: new Date(2019, 7, 15), name: "T-shirt", cost: 19 },
+  ];
   return (
     <div className="App">
       <h1>Test app</h1>
+      <CostItem
+        name={costs[0].name}
+        date={costs[0].date.toLocaleDateString("en-us", {
+          weekday: "long",
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+        cost={costs[0].cost}
+      />
+      <CostItem
+        name={costs[1].name}
+        date={costs[1].date.toLocaleDateString("en-us", {
+          weekday: "long",
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+        cost={costs[1].cost}
+      />
+      <CostItem
+        name={costs[2].name}
+        date={costs[2].date.toLocaleDateString("en-us", {
+          weekday: "long",
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+        cost={costs[2].cost}
+      />
     </div>
   );
 }
