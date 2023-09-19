6da1f41c7603f4a2cefba335e8997ff9e3fd5fd4
Ivan Tsatsura <tsatsura-ivan99@mail.ru>
2023-09-19 23:46:35 +0200

    Add states for input

--- src/Components/NewTransaction/TransactionForm.js
+++ src/Components/NewTransaction/TransactionForm.js
@@ -2,28 +2,46 @@ import "./TransactionForm.css";
 import React, { useState } from "react";
 
 function TransactionForm() {
+    const [isSpent, setIsSpent] = useState('');
+    const [amount, setAmount] = useState('');
+    const [description, setDescription] = useState('');
+    const [date, setDate] = useState('');
+
+    const isSpentChangeHandler = (event) => {
+        setIsSpent(event.target.value);
+    }
+    const descriptionChangeHandler = (event) => {
+        setDescription(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value);
+    }
+
   return (
     <form>
       <div className="transaction-form">
         <div className="spent-control">
           <label className="label">SPENT</label>
-          <input type="checkbox" />
+          <input className="check" type="checkbox" onChange={isSpentChangeHandler}/>
         </div>
         <div className="control">
           <label className="label">Amount</label>
-          <input type="number" />
+          <input className="row" type="number" onChange={amountChangeHandler}/>
         </div>
         <div className="control">
           <label className="label">Description</label>
-          <input type="text" />
+          <input className="row" type="text" onChange={descriptionChangeHandler}/>
         </div>
         <div className="control">
           <label className="label">Date</label>
-          <input type="date" />
+          <input className="row" type="date" onChange={dateChangeHandler}/>
         </div>
-        <div>
-          <button className="add-button">Add New</button>
-          <button className="cancel-button">Cancel</button>
+        <div className="buttons">
+          <button className="button add-button">Add New</button>
+          <button className="button cancel-button">Cancel</button>
         </div>
       </div>
     </form>
